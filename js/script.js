035de35e1dc2d8902b521c8ea17e5a2115343231
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-11-12 07:00:05 -0300

    Consumindo a API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 const pokemonName = document.getElementById('poke-name')
+const pokemonId = document.getElementById('poke-id')
+const pokemonImage = document.getElementById('poke-img')
+const form = document.querySelector('form')
+const inputSearch = document.querySelector('input')
 
 const callAPI = async (pokemon) =>{
     const API = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    const data = await API.json()
+    if(API.status == 200){
+        const data = await API.json()
+        return data
+    }
 }
+
+const renderPokemon = async (pokemon) =>{
+    pokemonName.innerHTML = 'Loading...'
+
+    const data = await callAPI(pokemon)
+
+    pokemonName.innerHTML = ''
+    if(data){
+        pokemonName.innerHTML = data.name;
+        pokemonId.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        inputSearch.value = ''
+    }
+    else{
+        pokemonName.innerHTML = 'Não encontrado'
+    }
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    console.log('Loading API..')
+    renderPokemon(inputSearch.value.toLocaleLowerCase())
+})
